ab11c9ebabd0d5ff0c81a0a1accb9de77fafdc83
Abu Raihan <raihanahmed01973@gmail.com>
2024-02-06 17:14:33 +0600

    responsive btn and search

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -6,12 +6,12 @@ const TodoForm = () => {
       <form action="" className="border p-6 mt-10 rounded-lg">
         <h2 className="text-center font-bold text-2xl">Create your todo</h2>
 
-        <div className="flex justify-between items-center mt-10">
+        <div className="flex md:justify-between md:flex-row flex-col items-center mt-10  gap-5">
           <h2 className="text-3xl font-bold text-blue-500">Your task</h2>
           <input
             type="text"
             placeholder="search your todo"
-            className="outline-none border-2 rounded-md px-3 focus:border-orange-500 py-2 w-[450px]"
+            className="outline-none border-2 rounded-lg px-3 focus:border-orange-500 py-2 lg:w-[450px] w-[280px]"
           />
           <div className="flex gap-3">
             <button className="btn bg-orange-600 text-white rounded-full px-5 py-2 hover:bg-orange-700 font-semibold">
